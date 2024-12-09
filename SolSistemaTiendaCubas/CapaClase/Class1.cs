994abb34ac603f4aec02e085008b9f0989d268bd
IsaacEmerson-05 <72278816@ijamvirtual.edu.pe>
2024-12-08 23:00:44 -0500

    Agregar archivos de proyecto.

---
+++ SolSistemaTiendaCubas/CapaClase/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace CapaClase
+{
+    public class Class1
+    {
+        public int idUsuario { get; get }
+        public string nombre { get; set; }
+        public string apellidos { get; set; }
+        public string tipoUsuario { get; set; }  //(Empleado, Gerente, Cliente)
+        public int dni { get; set; }
+        public int celular { get; set; }
+        public Boolean estado { get; set; }
+
+    }
+}
---
+++ SolSistemaTiendaCubas/CapaEntidades/entUsuario.cs
@@ -0,0 +1,14 @@
+﻿namespace CapaEntidades
+{
+    public class entUsuario
+    {
+        public int idUsuario { get; set; }
+        public string nombre { get; set; }
+        public string apellidos { get; set;  }
+        public string tipoUsuario { get; set;  } //(Empleado, Gerente, Cliente)
+        public int dni { get; set; }
+        public int celular { get; set; }
+        public Boolean estado { get; set; } //Habilitado o Inhabilitado
+
+    }
+}
